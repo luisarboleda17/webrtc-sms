edd6aa73b523571be95b1fb6664ce9d1347dd68c
luis_arboleda17 <hello@larboleda.io>
2020-06-27 10:29:34 -0500

    Added environment validation to activate request logs

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ const defaultConfig = require('./config');
  * @param config
  */
 module.exports = (config = null) => {
+  const appConfig = config || {
+    ...defaultConfig,
+    env: process.env.ENV || 'dev'
+  };
   const app = Hapi.server({
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'localhost',
-    app: config || {
-      ...defaultConfig,
-      env: process.env.ENV || 'dev'
-    }
+    app: appConfig
   });
 
   // Add requests logging
-  app.events.on('response', request => console.log(`${request.info.remoteAddress}: ${request.method.toUpperCase()} ${request.path} --> ${request.response.statusCode}`));
+  if (appConfig.env !== 'dev' && appConfig.env !== 'test') {
+    app.events.on('response', request => console.log(`${request.info.remoteAddress}: ${request.method.toUpperCase()} ${request.path} --> ${request.response.statusCode}`));
+  }
 
   // Register routes
   app.route(routes);
