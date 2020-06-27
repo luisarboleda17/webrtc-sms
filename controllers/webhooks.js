c413aed249122cf1f32750ca2be6a7c7d320beab
luis_arboleda17 <hello@larboleda.io>
2020-06-26 23:08:01 -0500

    Send sms invitation to group members added

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -3,6 +3,7 @@ const Nexmo = require('nexmo');
 const boom = require('@hapi/boom');
 
 const nexmoConfig = require('../config/nexmo');
+const smsConfig = require('../config/sms');
 const {sanitizeMessage} = require('../utils/sanitize');
 
 /**
@@ -12,17 +13,36 @@ const {sanitizeMessage} = require('../utils/sanitize');
  */
 const pollInboundSMS = (request, h) => {
   const nexmo = new Nexmo({
-    apiKey: nexmoConfig.apiKey
-  });
+    apiKey: nexmoConfig.apiKey,
+    apiSecret: nexmoConfig.apiSecret
+  }, {debug: process.env.ENV === 'local'});
   const userPhone = request.payload['msisdn'];
   const messageId = request.payload['messageId'];
   const messageText = sanitizeMessage(request.payload['text']);
   const receivedApiKey = request.payload['api-key'];
 
+  const friendsNumbers = ['50766734913']; // TODO: Get phone numbers from data source
+
   // Validate if same api key - TODO: Remove when signature verification is enabled
   if (receivedApiKey === nexmoConfig.apiKey) {
+
+    // Validate if it's a join me command
     if (messageText && messageText === 'join me') { // TODO: Change validation
-      return 'OK';
+      const invitationMessage = `${messageText}. ${smsConfig.pollCreationInstructions}`;
+
+      // Send message to group of people TODO: Add this process to Redis queue
+      friendsNumbers.forEach(phoneNumber => nexmo.message.sendSms(
+        nexmoConfig.senderPhoneNumber,
+        phoneNumber,
+        invitationMessage,
+        (error, response) => {
+          if (error) {
+            // TODO: Handle send error. Resend SMS to meet host
+          }
+        }
+      ));
+
+      return {status: 'poll_scheduled'};
     } else {
       return boom.badRequest('Unrecognized message');
     }
