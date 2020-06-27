1e20e47d9c3b46269438db23ac453a394642a1e6
luis_arboleda17 <hello@larboleda.io>
2020-06-27 11:49:32 -0500

    Tests restored after exposed key

---
+++ test/webhooks.test.js
@@ -0,0 +1,76 @@
+
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { afterEach, beforeEach, test, experiment } = exports.lab = Lab.script();
+
+const createApp = require('../app');
+const testConfig = require('./config');
+
+experiment('Webhooks ', () => {
+  let server;
+  const hostPhoneNumber = '50712345678';
+
+  beforeEach(() => {
+    server = createApp(testConfig);
+  });
+
+  afterEach(async () => await server.stop());
+
+  experiment('Route integrity', () => {
+    test('An invalid API key is received, then receive unauthorized', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/webhooks/poll',
+        payload: {
+          "msisdn": hostPhoneNumber,
+          "to": server.settings.app.nexmo.senderPhoneNumber,
+          "messageId": "16000002B6F4AC14",
+          "text": "Dear friends, will you join me at 8pm?",
+          "type": "text",
+          "keyword": "DEAR",
+          "api-key": `${server.settings.app.nexmo.apiKey}1`,
+          "message-timestamp": "2020-06-27 05:46:16"
+        }
+      });
+      expect(response.statusCode).to.equal(401);
+    });
+  });
+
+  experiment('Create a poll', () => {
+    test('A valid API key is received and join me command is included, then receive success', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/webhooks/poll',
+        payload: {
+          "msisdn": hostPhoneNumber,
+          "to": server.settings.app.nexmo.senderPhoneNumber,
+          "messageId": "16000002B6F4AC14",
+          "text": "Dear friends, will you join me at 8pm?",
+          "type": "text",
+          "keyword": "DEAR",
+          "api-key": server.settings.app.nexmo.apiKey,
+          "message-timestamp": "2020-06-27 05:46:16"
+        }
+      });
+      expect(response.statusCode).to.equal(200);
+    });
+
+    test('A valid API key is received and join me command is not included, then receive bad request', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/webhooks/poll',
+        payload: {
+          "msisdn": hostPhoneNumber,
+          "to": server.settings.app.nexmo.senderPhoneNumber,
+          "messageId": "16000002B6F4AC14",
+          "text": "Testing if this API it's vulnerable.",
+          "type": "text",
+          "keyword": "DEAR",
+          "api-key": server.settings.app.nexmo.apiKey,
+          "message-timestamp": "2020-06-27 05:46:16"
+        }
+      });
+      expect(response.statusCode).to.equal(400);
+    });
+  });
+});
