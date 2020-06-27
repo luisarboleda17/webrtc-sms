7c766f726ac5130658f7b29d5a60e76c6ece5f82
luis_arboleda17 <hello@larboleda.io>
2020-06-27 10:20:59 -0500

    Webhooks validation and routing moved to route handler

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -2,56 +2,44 @@
 const Nexmo = require('nexmo');
 const boom = require('@hapi/boom');
 
-const {sanitizeMessage} = require('../utils/sanitize');
-
 /**
- * Handle poll inbound sms webhook
+ * Handle create poll from SMS
  * @param request
  * @param h
+ * @returns {{data: boolean}}
  */
-const pollInboundSMS = (request, h) => {
-  const nexmoConfig = request.server.app.nexmo;
-  const pollConfig = request.server.app.poll;
+const createPollFromSMS = (request, h) => {
+  const pollConfig = request.server.settings.app.poll;
+  const nexmoConfig = request.server.settings.app.nexmo;
+
   const nexmo = new Nexmo({
     apiKey: nexmoConfig.apiKey,
     apiSecret: nexmoConfig.apiSecret
   }, {debug: request.server.app.env === 'local'});
 
-  const userPhone = request.payload['msisdn'];
-  const messageId = request.payload['messageId'];
-  const messageText = sanitizeMessage(request.payload['text']);
-  const receivedApiKey = request.payload['api-key'];
+  const messageText = request.payload['text'];
+  const invitationMessage = `${messageText}. ${pollConfig.pollCreationInstructions}`;
 
-  const friendsNumbers = ['50766734913']; // TODO: Get phone numbers from data source
+  const friendsNumbers = ['50766734913']; //Assuming we get phones from a data source TODO: Get phone numbers from data source
 
-  // Validate if same api key - TODO: Remove when signature verification is enabled
-  if (receivedApiKey === nexmoConfig.apiKey) {
-
-    // Validate if it's a join me command
-    if (messageText && messageText.toLowerCase().includes(pollConfig.pollCreateKeyword)) {
-      const invitationMessage = `${messageText}. ${pollConfig.pollCreationInstructions}`;
-
-      // Send message to group of people TODO: Add this process to Redis queue
-      friendsNumbers.forEach(phoneNumber => nexmo.message.sendSms(
-        nexmoConfig.senderPhoneNumber,
-        phoneNumber,
-        invitationMessage,
-        (error, response) => {
-          if (error) {
-            // TODO: Handle send error. Resend SMS to meet host
-          }
+  // Only send SMS if it's enabled
+  if (nexmoConfig.sendSmsEnabled) {
+    // Send message to number's friends TODO: Add this process to a queue
+    friendsNumbers.forEach(phoneNumber => nexmo.message.sendSms(
+      nexmoConfig.senderPhoneNumber,
+      phoneNumber,
+      invitationMessage,
+      (error, response) => {
+        if (error) {
+          // TODO: Handle send error. Resend SMS to meet host
         }
-      ));
-
-      return {data: true};
-    } else {
-      return boom.badRequest('Unrecognized message');
-    }
-  } else {
-    return boom.unauthorized('Invalid API Key');
+      }
+    ));
   }
+
+  return {data: true};
 };
 
 module.exports = {
-  pollInboundSMS
+  createPollFromSMS
 };
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,10 +1,30 @@
 
+const Boom = require('@hapi/boom');
+
 const {webhooks: webhooksControllers} = require('../controllers');
 
 module.exports = [
   {
     method: 'POST',
     path: '/webhooks/poll',
-    handler: webhooksControllers.pollInboundSMS
+    handler: (request, h) => {
+      const nexmoConfig = request.server.settings.app.nexmo;
+      const receivedApiKey = request.payload['api-key'];
+
+      // Validate API key
+      if (receivedApiKey === nexmoConfig.apiKey) {
+        const pollConfig = request.server.settings.app.poll;
+        const messageText = request.payload['text'].toLowerCase();
+
+        // Route to controller
+        if (messageText.includes(pollConfig.pollCreateKeyword)) {
+          return webhooksControllers.createPollFromSMS(request, h);
+        } else {
+          return Boom.badRequest('Unrecognized action');
+        }
+      } else {
+        return Boom.unauthorized('Invalid API Key');
+      }
+    }
   }
 ];
