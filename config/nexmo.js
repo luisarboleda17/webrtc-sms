80ad2f9d7b418f0d987c001f296c2a3c3d44b2ed
luis_arboleda17 <hello@larboleda.io>
2020-06-27 10:20:12 -0500

    Nexmo enable sms config validation fixed

--- config/nexmo.js
+++ config/nexmo.js
@@ -4,6 +4,7 @@
  * @type {{API_KEY}}
  */
 module.exports = {
+  sendSmsEnabled: process.env.SEND_SMS_ENABLED === '1',
   apiKey: process.env.NEXMO_API_KEY,
   apiSecret: process.env.NEXMO_API_SECRET,
   senderPhoneNumber: process.env.NEXMO_PHONE_NUMBER
