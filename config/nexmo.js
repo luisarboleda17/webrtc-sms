db11dcccc203cc0cfc137ec72e58abc106f729d5
luis_arboleda17 <hello@larboleda.io>
2020-06-26 23:01:31 -0500

    Added  missing nexmo configurations

--- config/nexmo.js
+++ config/nexmo.js
@@ -4,5 +4,7 @@
  * @type {{API_KEY}}
  */
 module.exports = {
-  apiKey: process.env.NEXMO_API_KEY
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET,
+  senderPhoneNumber: process.env.NEXMO_PHONE_NUMBER
 };
