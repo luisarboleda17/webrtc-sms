0f9bc9cbc8a114805ae50c8b1632085aecebea48
luis_arboleda17 <hello@larboleda.io>
2020-06-26 23:07:33 -0500

    Poll creation instructions on config grammar fixed

--- config/sms.js
+++ config/sms.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-  pollCreationInstructions: 'Please respond with Yes or No if you can join me'
+  pollCreationInstructions: 'Please respond with Yes or No if you can join me!'
 };
