049028266b154b3fa27c6c4dfecadee45a45fd39
luis_arboleda17 <hello@larboleda.io>
2020-06-28 18:59:20 -0500

    Generate token method added to opentok service

--- services/opentok.js
+++ services/opentok.js
@@ -17,6 +17,29 @@ const createSession = (apiKey, apiSecret) => new Promise(
   }
 );
 
+/**
+ * Generate an OpenTok token to a session
+ * @param apiKey
+ * @param apiSecret
+ * @param sessionId
+ * @param role
+ * @param expirationTime
+ * @param name
+ * @returns {*}
+ */
+const generateToken = (
+  apiKey,
+  apiSecret,
+  sessionId,
+  role='subscriber',
+  expirationTime= (new Date().getTime() / 1000)+(7 * 24 * 60 * 60),
+  name=null
+) => {
+  const opentok = new OpenTok(apiKey, apiSecret);
+  return opentok.generateToken(sessionId, {role: role, expireTime: expirationTime, data: `name=${name}`});
+};
+
 module.exports = {
-  createSession
+  createSession,
+  generateToken
 };
