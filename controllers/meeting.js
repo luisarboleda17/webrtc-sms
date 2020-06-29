32a3a2ddd6cff42f23e4506c6502074a7998431f
luis_arboleda17 <hello@larboleda.io>
2020-06-28 23:36:09 -0500

    Meeting controller and get token for meeting method created

---
+++ controllers/meeting.js
@@ -0,0 +1,33 @@
+
+const Boom = require('@hapi/boom');
+
+const { meeting: meetingService, opentok: openTokService } = require('../services');
+
+/**
+ * Generate client token for meeting session
+ * @param request
+ * @param h
+ * @returns {Promise<Boom<unknown>|{data: {token: *}}>}
+ */
+const generateTokenForMeeting = async (request, h) => {
+  const opentokConfig = request.server.settings.app.opentok;
+  const meetingId = request.params.meetingId;
+
+  try {
+    const meeting = await meetingService.getMeetingById(meetingId);
+    const token = await openTokService.generateToken(
+      opentokConfig.apiKey,
+      opentokConfig.apiSecret,
+      meeting.openTokSessionId,
+      'publisher'
+    );
+    return { data: { token } };
+  } catch(err) {
+    console.error(err);
+    return Boom.internal();
+  }
+};
+
+module.exports = {
+  generateTokenForMeeting
+};
